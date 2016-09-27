826501c8309948ec2ed46e43b59df5a797f17bcc
Jeremy Beaulieu <jbeaulieu@nimbios.org>
2016-09-27 08:41:44 +0000

    version 1.20

--- R/ancRECON.R
+++ R/ancRECON.R
@@ -251,10 +251,10 @@ ancRECON <- function(phy, data, p, method=c("joint", "marginal", "scaled"), hrm=
 
 	p[p==0] = exp(-21)
 	Q[] <- c(p, 0)[rate]
+    diag(Q) <- -rowSums(Q)
 	phy <- reorder(phy, "pruningwise")
 	TIPS <- 1:nb.tip
 	anc <- unique(phy$edge[,1])
-	
 	if(method=="joint"){
 		lik.states<-numeric(nb.tip + nb.node)
 		comp<-matrix(0,nb.tip + nb.node,ncol(liks))
